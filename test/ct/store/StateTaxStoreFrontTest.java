eccf49bd42cbe7c9c1d0d079347fd3767c8f0e3e
Eric OConnor <eric.oconnor@gmail.com>
2022-07-03 22:37:21 -0700

    Added more test cases for tax-free states -- for states not starting with an "M"

--- test/ct/store/StateTaxStoreFrontTest.java
+++ test/ct/store/StateTaxStoreFrontTest.java
@@ -60,6 +60,37 @@ public class StateTaxStoreFrontTest
     }
 
     @Test
+    public void storeTest1CA()
+    {
+        User customer = new Customer("1", new UserInfo("f", "", "l", "123 testing way", "emma" , "CA", "12345"));
+
+        sf.addItemToShoppingCart(customer, "1", 1);
+        sf.addItemToShoppingCart(customer, "2", 1);
+        sf.addItemToShoppingCart(customer, "3", 1);
+
+        Receipt rec = sf.checkout(customer);
+
+        for(CartItem item: rec.getItemList()) {
+            switch(item.getProduct().getId()) {
+                case "1":
+                    assertEquals(12.49f, item.getTotal());
+                    break;
+                case "2":
+                    assertEquals(16.49f, item.getTotal());
+                    break;
+                case "3":
+                    assertEquals(0.85f, item.getTotal());
+                    break;
+                default:
+                    fail("Invalid item added to the cart!");
+            }
+        }
+
+        assertEquals(1.50f, rec.getTotalTax());
+        assertEquals(29.83f, rec.getGrandTotal());
+    }
+
+    @Test
     public void storeTest2MN()
     {
         User customer = new Customer("1", new UserInfo("f", "", "l", "123 testing way", "emma" , "MN", "12345"));
@@ -119,5 +150,67 @@ public class StateTaxStoreFrontTest
 
         assertEquals(1.95f, rec.getTotalTax());
         assertEquals(69.93f, rec.getGrandTotal());
+    }
+
+    @Test
+    public void storeTest2CA()
+    {
+        User customer = new Customer("1", new UserInfo("f", "", "l", "123 testing way", "emma" , "CA", "12345"));
+
+        sf.addItemToShoppingCart(customer, "4", 1);
+        sf.addItemToShoppingCart(customer, "5", 1);
+
+        Receipt rec = sf.checkout(customer);
+
+        for(CartItem item: rec.getItemList()) {
+            switch(item.getProduct().getId()) {
+                case "4":
+                    assertEquals(10.50f, item.getTotal());
+                    break;
+                case "5":
+                    assertEquals(54.65f, item.getTotal());
+                    break;
+                default:
+                    fail("Invalid item added to the cart!");
+            }
+        }
+
+        assertEquals(7.65f, rec.getTotalTax());
+        assertEquals(65.15f, rec.getGrandTotal());
+    }
+
+    @Test
+    public void storeTest3CA()
+    {
+        User customer = new Customer("1", new UserInfo("f", "", "l", "123 testing way", "emma" , "CA", "12345"));
+
+        sf.addItemToShoppingCart(customer, "6", 1);
+        sf.addItemToShoppingCart(customer, "7", 1);
+        sf.addItemToShoppingCart(customer, "8", 1);
+        sf.addItemToShoppingCart(customer, "9", 1);
+
+        Receipt rec = sf.checkout(customer);
+
+        for(CartItem item: rec.getItemList()) {
+            switch(item.getProduct().getId()) {
+                case "6":
+                    assertEquals(32.19f, item.getTotal());
+                    break;
+                case "7":
+                    assertEquals(20.89f, item.getTotal());
+                    break;
+                case "8":
+                    assertEquals(9.75f, item.getTotal());
+                    break;
+                case "9":
+                    assertEquals(11.80f, item.getTotal());
+                    break;
+                default:
+                    fail("Invalid item added to the cart!");
+            }
+        }
+
+        assertEquals(6.65f, rec.getTotalTax());
+        assertEquals(74.63f, rec.getGrandTotal());
     }
 }
